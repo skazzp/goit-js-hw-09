b56929062235e96e6895fdffe6bbcb24194419e7
skazzp <skazzp@gmail.com>
2022-08-15 01:26:18 +0300

    t2 alert done

---
+++ src/js/02-timer.js
@@ -0,0 +1,40 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const startButton = document.querySelector('button[data-start]');
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+const date = new Date();
+console.log('Date: ', convertMs(date));
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    // console.log(convertMs(selectedDates[0]));
+    if (selectedDates[0] > date) console.log('123');
+    else {
+      window.alert('Please choose a date in the future');
+    }
+  },
+};
+flatpickr('#datetime-picker', options);
